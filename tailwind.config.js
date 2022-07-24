bcfcc9b655b5f82f7d9912a06b3a693ec40c40bf
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-07-24 23:46:45 +0900

    Tailwind CSS使用のため、使用するパスを設定
    tailwind.config.jsのcontentに適用するパスを指定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
