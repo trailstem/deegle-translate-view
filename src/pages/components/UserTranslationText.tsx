72787e69c77623489474239e4bf5afba5fe614a8
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-08-01 00:12:30 +0900

    フッターの作成（Footer.tsx） デザイン修正
    テキストエリア等のデザインの修正、Footerの作成をしました。

--- src/pages/components/UserTranslationText.tsx
+++ src/pages/components/UserTranslationText.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 export const UserTranslationText = (props: any) => {
   const {inputText, translate} = props;
   return (
-            <div className="">
-              <textarea id="" onChange={translate} className='border-4 md:w-full w-36 h-28 ms:h-56 
+            <div className="sm:flex justify-center ms:px-24">
+              <textarea id="" onChange={translate} className='border-4 sm:w-1/3 w-full h-28 sm:h-56  md:h-56 mx-8 
                rounded-lg' value={inputText}></textarea>
             </div>
   );
