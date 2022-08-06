e404c29df9df53d587fd4c5e1057aabe7fd1e0cd
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-08-06 17:59:46 +0900

    プルリク（2022/08/02）の修正
    下記ソースのインデント・タイポ修正しました。
    src/pages/components/Footer.tsx
    src/pages/components/Header.tsx
    src/pages/components/LanguageSwichingTab.tsx
    src/pages/components/UserTranslationText.tsx
    src/pages/index.tsx

--- src/pages/components/UserTranslationText.tsx
+++ src/pages/components/UserTranslationText.tsx
@@ -5,11 +5,10 @@ export const UserTranslationText = (props: any) => {
   const {inputText, translate} = props;
   return (
             <div className="sm:flex justify-center ms:px-24">
-              <textarea id="" onChange={translate} className='border-4 sm:w-1/3 w-full h-28 sm:h-56  md:h-56 mx-8 
-               rounded-lg' value={inputText}></textarea>
+              <textarea id="" onChange={translate} className='border-4 sm:w-1/3 w-full h-28 sm:h-56  md:h-56 mx-8 rounded-lg' value={inputText}>
+              </textarea>
             </div>
   );
-
 }
 
 export default UserTranslationText;
