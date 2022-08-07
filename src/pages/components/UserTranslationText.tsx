e83c8c5b26bd682aa2b73daf88163ac9154606df
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-08-07 15:03:13 +0900

    不要なインデント修正
    不要なインデントを削除しました。

--- src/pages/components/UserTranslationText.tsx
+++ src/pages/components/UserTranslationText.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-export const UserTranslationText = (props: any) => {
+export const UserTranslationText = (props : any): JSX.Element => {
   const {inputText, translate} = props;
   return (
-            <div className="sm:flex justify-center ms:px-24">
-              <textarea id="" onChange={translate} className='border-4 sm:w-1/3 w-full h-28 sm:h-56  md:h-56 mx-8 rounded-lg' value={inputText}>
-              </textarea>
-            </div>
+    <div className="flex justify-center text-center px-4 py-6 m-2  boder-2 rounded-md">
+      <textarea id="" onChange={translate} className='outline-none sm:w-1/3 w-full h-28 sm:h-56  md:h-56 border-2 rounded-lg' value={inputText}>
+      </textarea>
+    </div>
   );
 }
 
