9850074a5a7ba8f2a515858c9e9913e441801c77
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-09-11 13:36:07 +0900

    componentsフォルダ、home.module.css修正
    component配下にAtomic design構成作成
    home.module.cssが不要だった為、削除

--- src/pages/components/UserTranslationText.tsx
+++ src/pages/components/UserTranslationText.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { memo } from "react"
 
 export const UserTranslationText = memo((props : any): JSX.Element => {
-  const {inputText, translate} = props;
+  const {inputText, setInputText, translate} = props;
+  
+  const change = (inputText: any) => {
+    console.log(inputText)
+    setInputText(translate(inputText))
+  }
+
   return (
     <div className="flex justify-center text-center px-4 py-6 m-2  boder-2 rounded-md">
-      <textarea id="" onChange={translate} className='outline-none sm:w-1/3 w-full h-28 sm:h-56  md:h-56 border-2 rounded-lg'>{inputText}
+      <textarea id="a" onChange={change} className='outline-none sm:w-1/3 w-full h-28 sm:h-56  md:h-56 border-2 rounded-lg'>{inputText}
       </textarea>
     </div>
   );
