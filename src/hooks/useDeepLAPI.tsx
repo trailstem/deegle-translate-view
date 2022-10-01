2e418608844f5305b6f3c3f9e62ba3df2b39325c
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-10-01 11:22:12 +0900

    async/awaitを使用し、コードの可読性向上
    async/awaitを使用して、コードの可読性を上げました

--- src/hooks/useDeepLAPI.tsx
+++ src/hooks/useDeepLAPI.tsx
@@ -1,37 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export  const DeepLAPI = async (inputText: any, setAftTranDeepl: any) : Promise<void> => {
-
-  console.log("--------");
-  console.log(inputText.target.value);
-  console.log("--------");
+//テキストを受け取り、DeepLAPIで翻訳する関数
+export  const DeepLAPI = async(text: string) => {
 
   const API_Key = process.env.NEXT_PUBLIC_DeepL_API_KEY;
   const API_URL = "https://api-free.deepl.com/v2/translate";
-
   let content = encodeURI(
     "auth_key=" +
       API_Key +
       "&text=" +
-      inputText.target.value +
-      "&source_lang=JA&target_lang=EN"
+      text +
+      "&source_lang=EN&target_lang=JA"
+      // "&source_lang=JA&target_lang=EN"
   );
-
   let url = API_URL + "?" + content;
 
-  await fetch(url)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error(
-        `Could not reach the API:${API_Key} `+ response.statusText
-      );
-    }
-  })
-  .then((data) => {
-    let tran = data["translations"][0]["text"];
-      setAftTranDeepl(tran);
-  })
+    const data = await fetch(url).then(response => {  return response.json(); });
+    let tran: string = data["translations"][0]["text"];
+    return tran;
 }
 export default DeepLAPI
