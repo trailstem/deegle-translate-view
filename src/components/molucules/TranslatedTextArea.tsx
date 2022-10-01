8da2a54785486d0f15164aee9afe0a29b0119b6b
github user tmcoco0228si <proguteinitar3150@gmail.com>
2022-10-01 11:24:31 +0900

    index.tsxに記述していた各コンポーネントをatomsフォルダ、layoutフォルダに移動

---
+++ src/components/molucules/TranslatedTextArea.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback, useRef }  from "react";
+import { useState } from "react";
+import DeepLAPI from "../../hooks/useDeepLAPI";
+import GoogleAPI from "../../hooks/useGoogleAPI";
+import InputTextArea from "../atoms/InputTextArea";
+import OutputTextArea from "../atoms/OutputTextArea";
+
+export const TranslatedTextArea = (props: any) => {
+      //ユーザの入力値
+      const [inputText, setUserInputText] = useState<string>("");
+
+      // const inputText = useRef<string>(null);
+      const [googleText, setGoogleText] = useState<string>("");
+
+      const [deepLText, setDeepLText] = useState<string>("");
+      
+      const translate = useCallback((e: any) => {
+        setTimeout(async () => {
+          setUserInputText(e);
+          const gText = await GoogleAPI(e);
+          const dText = await DeepLAPI(e);
+          setGoogleText(gText);
+          setDeepLText(dText);
+          // alert("翻訳完了しました");
+        }, 800)
+      }, [inputText]);
+
+  return (
+    <>
+      <div className="flex justify-center text-center px-4 py-6 m-2  boder-2 rounded-md">
+      <InputTextArea inputText={inputText} translate={translate}/>
+      </div>
+      <OutputTextArea translatedText={googleText} color="blue"/>
+      <OutputTextArea translatedText={deepLText} color="yellow"/>
+    </>
+  )
+}
