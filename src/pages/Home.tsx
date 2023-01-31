88a52bc6f8aba865ef506d9cb07c6903ebdf5cd6
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-01 00:38:46 +0900

    不要なクラス名、コメント、importを削除

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,27 @@
-import { memo } from "react"
+import { memo } from "react";
 
-import Footer from '../components/organisms/layout/Footer';
+import Footer from "../components/organisms/layout/Footer";
 import Header from "../components/organisms/layout/Header";
 import { TranslatedTextArea } from "../components/molucules/TranslatedTextArea";
 import { SwichLanguageArea } from "../components/molucules/SwitchLanguageArea";
-import { ThumbsUpIcon } from "../components/atoms/ThumbUpIcon";
-import { TextCopyIcon } from "../components/atoms/TextCopyIcon";
+import { DefaultDialg } from "../components/atoms/DefaultDialog";
 
 export const Home = memo(() => {
   return (
     <>
-    <Header />
-        <SwichLanguageArea />
+      <div className="overflow-auto">
+        <Header />
+        <div className="flex justify-center my-5">
+          <SwichLanguageArea />
+        </div>
         <TranslatedTextArea />
-        <TextCopyIcon />
-        <ThumbsUpIcon/>
-    <Footer />
+
+        <div className="flex justify-center my-5">
+          <DefaultDialg />
+        </div>
+        <Footer />
+      </div>
     </>
-  )
-})
+  );
+});
+export default Home;
