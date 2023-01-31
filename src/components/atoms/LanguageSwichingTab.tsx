3b456f073e55c859f4d1fa511edb1d539ccbdb93
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-01 00:38:46 +0900

    不要なクラス名、コメント、importを削除

--- src/components/atoms/LanguageSwichingTab.tsx
+++ src/components/atoms/LanguageSwichingTab.tsx
@@ -1,35 +1,13 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { memo } from "react"
 
 import { Select, Option } from "@material-tailwind/react";
 
 const LanguageSwichingTab = memo((): JSX.Element => {
 
-  // 規模が小さいことを想定し、日本語と英語のみ対応するため固定値
-  // const data = [
-  //   {
-  //     label: "Japanese",
-  //     value: "JA",
-  //   },
-  //   {
-  //     label: "English",
-  //     value: "EN",
-  //   },
-  // ];
-
-  // //不要なレンダリングが発生しないようにuseRef使用
-  // const languageData = useRef(data);
-  // const changeTab = (e: any) => {
-  //   // if (e.dataset.value === "EN") {
-  //   //   e.data_value == "JA";
-  //   // }
-  //   console.log("changeTab", e.dataset.value);
-  // }
-
   return (
-    <div className="flex w-full items-end gap-4">
+    <div className="flex items-end gap-4 mt-10">
       <Select variant="standard" label="">
-        <Option>{null}</Option>
         <Option value="JA">日本語</Option>
         <Option value="EN">英語</Option>
       </Select>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { memo } from 'react';
 
 
@@ -5,7 +6,9 @@ const Header = memo((): JSX.Element => {
   return (      
     <header className="bg-blue-600  text-white p-4">
       <nav className="flex justify-between mx-auto container items-center">
-        <div className="text-2xl sm:text-4xl font-bold">Deegle</div>
+      <div className="text-2xl sm:text-4xl font-bold">
+        <Link href="/">Deegle</Link></div>
+
           <div className='sm:space-x-12 sm:text-2xl'>
             <a href="" className="hover:text-green-200 transision-all duration-300 pr-1">ホーム</a>
             <a href="" className="hover:text-green-200 transision-all duration-300">問い合わせ</a>
