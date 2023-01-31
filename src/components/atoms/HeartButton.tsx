47afe7d745be819eeba514d8d3eae50abbbfe475
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-01 00:39:57 +0900

    APIサーバに接続しJSONの値を取得するためのcomponent作成

--- src/components/atoms/HeartButton.tsx
+++ src/components/atoms/HeartButton.tsx
@@ -1,20 +1,29 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { memo } from "react";
+import { memo, useRef } from "react";
+import { useState } from "react";
 import { IconButton } from "@material-tailwind/react";
 import { ThumbsUpIcon } from "./ThumbUpIcon";
+import axios from "axios";
+
 
 
 export const HeartButton = memo((props: any) => {
-  const { color } = props;
+
+  const [good, setGood] = useState<string>("");const { color } = props;
+  
+  const handleSubmit = async (event: { preventDefault: () => void; }): Promise<any>  => {
+    event.preventDefault();
+    const res = await axios.get("http://localhost:8080/test")
+    const item = res.data["message"];
+    setGood(item);
+    console.log(good);
+  }
+
+
   return (
     <div className="flex md:justify-center mt-4">
-      <IconButton color={color}> 
-        <ThumbsUpIcon/>
-      </IconButton>
+        <IconButton onClick={handleSubmit} type="submit" color={color} > 
+          <ThumbsUpIcon/>
+        </IconButton>
     </div>
   );
 })
-
-
-
-
