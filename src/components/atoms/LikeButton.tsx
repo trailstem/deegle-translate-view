5a54b0e743fe8aa956699e6f8b5bd43cc6e50096
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-07 07:43:42 +0900

    いいねボタンをダイアログで一旦対応 いいねボタンを用意していたがダイアログ（Button）で対応予定

---
+++ src/components/atoms/LikeButton.tsx
@@ -0,0 +1,29 @@
+import { memo, useRef } from "react";
+import { useState } from "react";
+import { IconButton } from "@material-tailwind/react";
+import { ThumbsUpIcon } from "./ThumbUpIcon";
+import axios from "axios";
+
+
+
+export const HeartButton = memo((props: any) => {
+
+  const [good, setGood] = useState<string>("");const { color } = props;
+  
+  const handleSubmit = async (event: { preventDefault: () => void; }): Promise<any>  => {
+    event.preventDefault();
+    const res = await axios.get("http://localhost:8080/test")
+    const item = res.data["likeCount"];
+    setGood(item);
+    console.log(good);
+  }
+
+
+  return (
+    <div className="flex md:justify-center mt-4">
+        <IconButton onClick={handleSubmit} type="submit" color={color} > 
+          <ThumbsUpIcon/>
+        </IconButton>
+    </div>
+  );
+})
