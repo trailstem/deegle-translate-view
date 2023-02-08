a787952ccb82327c1d5dfa9c1d17fa12928dbc22
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-08 22:40:20 +0900

    言語selectbox変更時に伴う各コンポーネントの修正

--- src/components/molucules/SwitchLanguageArea.tsx
+++ src/components/molucules/SwitchLanguageArea.tsx
@@ -1,18 +1,21 @@
-import { memo } from "react";
+import { memo, useContext } from "react";
 import LanguageSwichingTab from "../atoms/LanguageSwichingTab";
-import { ArrowsRightLeftIcon } from '@heroicons/react/24/solid'
+import { ArrowsRightLeftIcon } from "@heroicons/react/24/solid";
 
-export const SwichLanguageArea = memo(() => {
+import { FromToContext } from "../../pages/home";
+
+export const SwichLanguageArea = memo((props: any): JSX.Element => {
+  const context = useContext(FromToContext);
+  const {fromLang, setFromLang, toLang,setToLang } = context;
   return (
     <>
-    <div className="w-2/12">
-      <div className="flex justify-center my-5">
-      <LanguageSwichingTab/>
-        <ArrowsRightLeftIcon className="h-12 w-12 text-blue-500"/>
-      <LanguageSwichingTab/>
-    </div>
-    </div>
-
+      <div className="w-2/12">
+        <div className="flex justify-center my-5">
+          <LanguageSwichingTab onChange={setFromLang}  value={fromLang}/>
+          <ArrowsRightLeftIcon className="h-12 w-12 text-blue-500" />
+          <LanguageSwichingTab onChange={setToLang} value={toLang} />
+        </div>
+      </div>
     </>
-  )
+  );
 });
